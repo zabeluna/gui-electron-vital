9417b1b092c031503e6dd0f3fea77400f0cef894
izabelle <personalife503@gmail.com>
2024-11-16 22:33:15 -0300

    fix: dashboard

--- src/components/sections/dashboard.tsx
+++ src/components/sections/dashboard.tsx
@@ -5,36 +5,30 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card"
-import { GraphicsCard } from "./graphics-card"
-import { Button } from "@/components/ui/button"
+} from '@/components/ui/card';
+import { GraphicsCard } from './graphics-card';
+import { Button } from '@/components/ui/button';
 
-
-
-export function Dashboard () {
-  return(
+export function Dashboard() {
+  return (
     <div className="ml-28 mt-5">
       <Card className="bg-white text-black h-80">
-  <CardHeader>
-    <CardTitle>Card Title</CardTitle>
-    <CardDescription>Card Description</CardDescription>
-  </CardHeader>
-  <CardContent>
-
-    <div className="grid justify-end">
-      <button className="bg-green-500 rounded-md h-9 w-16"></button>
-      <button className="bg-green-500 rounded-md h-9 w-16 mt-2"></button>
-      <button className="bg-green-500 rounded-md h-9 w-16 mt-2"></button>
-      <button className="bg-green-500 rounded-md h-9 w-16 mt-2"></button>
+        <CardHeader>
+          <CardTitle>Card Title</CardTitle>
+          <CardDescription>Card Description</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <div className="grid justify-end">
+            <button className="bg-green-500 rounded-md h-9 w-16"></button>
+            <button className="bg-green-500 rounded-md h-9 w-16 mt-2"></button>
+            <button className="bg-green-500 rounded-md h-9 w-16 mt-2"></button>
+            <button className="bg-green-500 rounded-md h-9 w-16 mt-2"></button>
+          </div>
+        </CardContent>
+        <CardFooter></CardFooter>
+      </Card>
+      <div></div>
+      <GraphicsCard />
     </div>
-  </CardContent>
-  <CardFooter>
-
-  </CardFooter>
-</Card>
-<div></div>
-<GraphicsCard/>
-
-    </div>
-  )
+  );
 }
